3ffa913157a9cd099035f00f6dfc4e20fd2dc279
Crowdbotics <team@crowdbotics.com>
2020-07-12 16:05:52 +0000

    Initial commit.

---
+++ backend/par_time_18845/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for par_time_18845 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'par_time_18845.settings')
+
+application = get_wsgi_application()
